ec2159f2bde36a4c918a21eb786822dc050e5ad1
Yogas Karnik <yogas.karnik@gmail.com>
2023-11-13 21:18:07 +0100

    Initial commit

---
+++ src/main/java/com/wtm/superheroapi/SuperheroApiApplication.java
@@ -0,0 +1,13 @@
+package com.wtm.superheroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtm/superheroapi/SuperheroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtm.superheroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
